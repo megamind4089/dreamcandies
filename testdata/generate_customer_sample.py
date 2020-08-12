cfdf90b00051884255791bf3cf05602ae9b0bf8a
Mega Mind <68985133+megamind4089@users.noreply.github.com>
2020-08-13 02:52:46 +0800

    Update test data generate script to generate incremental data

--- testdata/generate_customer_sample.py
+++ testdata/generate_customer_sample.py
@@ -5,10 +5,13 @@ no_of_entries = 1000
 with open('sample.csv', 'w', encoding='utf-8') as out_file:
     print("Writing customer_sample File")
     num = 12345
+    num_range = 1000
     output = '"CUSTOMER_CODE"\n'
     out_file.write(output)
     for i in range(no_of_entries):
         output=""
-        output += '"CUST' + str(randrange(num, num + 500000)).zfill(10) + '"\n'
+        output += '"CUST' + str(randrange(num, num + num_range)).zfill(10) + '"\n'
+        num = num + 100
+        num_range = num_range + 100
         out_file.write(output)
 print("Writing customer_sample File Completed")
